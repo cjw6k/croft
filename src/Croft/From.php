19b9d6783dd8012438634c086b251aa92c087317
cjw6k <chris@lot2k.com>
2022-08-20 01:27:37 -0300

    fromdir

--- src/Croft/From.php
+++ src/Croft/From.php
@@ -1,9 +1,23 @@
 <?php
 
-// Make various paths available in globally defined constants
-const SOURCE_ROOT = __DIR__ . '/';
-const PACKAGE_ROOT = SOURCE_ROOT . '../';
-const CONTENT_ROOT = PACKAGE_ROOT . 'content/';
-const TEMPLATES_DEFAULT = PACKAGE_ROOT . 'templates/default/';
-const TEMPLATES_LOCAL = PACKAGE_ROOT . 'templates/local/';
-const VAR_ROOT = PACKAGE_ROOT . 'var/';
+namespace Croft;
+
+use A6a\FromDir\AsDirectoryInPackageRootWithBackedEnum;
+use A6a\FromDir\NamesDirectoryInPackageRoot;
+
+enum From: string implements NamesDirectoryInPackageRoot
+{
+    use asDirectoryInPackageRootWithBackedEnum;
+
+    case ___ = '___';
+    case ART = 'art';
+    case BOOTSTRAP = 'bootstrap';
+    case CONFIG = 'config';
+    case CONTENT = 'content';
+    case SRC = 'src';
+    case TEMPLATES___DEFAULT = 'templates___default';
+    case TEMPLATES___LOCAL = 'templates___local';
+    case TESTS___FIXTURES = 'tests___Fixtures';
+    case VAR = 'var';
+    case VENDOR = 'vendor';
+}
