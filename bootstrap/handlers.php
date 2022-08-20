e229edeeb6614eecc841a2554453a2272dba3d75
cjw6k <chris@lot2k.com>
2022-08-20 16:49:50 -0300

    tidy up

--- bootstrap/handlers.php
+++ bootstrap/handlers.php
@@ -5,7 +5,5 @@ use Dice\Dice;
 
 $bootstraps = (new Dice())
     ->addRules(require From::CONFIG->dir() . 'handlers/collision.php')
-    // log isn't ready
-    ->addRules(require From::CONFIG->dir() . 'handlers/log.php');
 
 $bootstraps->create('$nmCollision');
