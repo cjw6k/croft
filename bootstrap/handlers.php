2632b1ef768ca1cfa35ded75f26425fbb59f760c
cjw6k <chris@lot2k.com>
2022-08-20 18:57:15 -0300

    whoops

--- bootstrap/handlers.php
+++ bootstrap/handlers.php
@@ -3,7 +3,12 @@
 use Croft\From;
 use Dice\Dice;
 
-$bootstraps = (new Dice())
-    ->addRules(require From::CONFIG->dir() . 'handlers/collision.php');
+$bootstrapper = function(string $name, string $handlerPath) {
+    $boots = (new Dice())->addRules(require From::CONFIG->dir() . "handlers/{$handlerPath}.php");
+    $boots->create($name);
+};
 
-$bootstraps->create('$nmCollision');
+match (PHP_SAPI) {
+    'cli' => $bootstrapper('$nmCollision', 'collision'),
+    default => $bootstrapper('$whoops', 'whoops'),
+};
