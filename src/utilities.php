9d85cc422ff00ad142966ea50591bc579c8b1704
cjw6k <chris@lot2k.com>
2022-08-20 01:01:16 -0300

    phpcs

--- src/utilities.php
+++ src/utilities.php
@@ -5,48 +5,50 @@
  *
  * @param mixed $data The data to display.
  *
- * @return void
- *
  * @psalm-suppress ForbiddenCode
  *
  * @SuppressWarnings(PHPMD.DevelopmentCodeFragment)
  */
-function showMe($data)
+function showMe(mixed $data): void
 {
     // Determine which function called this method
     $trace = debug_backtrace();
 
     // Determine appropriate newline character
     $newline = PHP_EOL;
-    if('cli' != php_sapi_name()) {
+
+    if (php_sapi_name() != 'cli') {
         $newline = '<br>';
 
         // If not in CLI, use HTML
         echo '<pre>';
     }
 
-    if(isset($trace[0]['file']) && isset($trace[0]['line'])) {
+    if (isset($trace[0]['file']) && isset($trace[0]['line'])) {
         echo 'DEBUG: ', $trace[0]['file'], ', Line ', $trace[0]['line'], $newline, $newline;
     }
+
     var_dump($data);
     echo $newline;
 
     // If not in CLI, use HTML
-    if('cli' != php_sapi_name()) {
-        echo '</pre>';
+    if (php_sapi_name() == 'cli') {
+        return;
     }
+
+    echo '</pre>';
 }
 
 /**
  * Get the time the current request was made, or from the system clock if request time is not set
  *
- * @return integer The current time as a unix time stamp.
+ * @return int The current time as a unix time stamp.
  *
  * @SuppressWarnings(PHPMD.Superglobals)
  */
-function now()
+function now(): int
 {
-    if(isset($_SERVER['REQUEST_TIME'])) {
+    if (isset($_SERVER['REQUEST_TIME'])) {
         return $_SERVER['REQUEST_TIME'];
     }
 
@@ -60,13 +62,13 @@ function now()
  *
  * @return mixed The data with no nested objects.
  */
-function obj2arr($data)
+function obj2arr(mixed $data): mixed
 {
-    if(is_object($data)) {
+    if (is_object($data)) {
         $data = get_object_vars($data);
     }
 
-    if(is_array($data)) {
+    if (is_array($data)) {
         return array_map('obj2arr', $data);
     }
 
